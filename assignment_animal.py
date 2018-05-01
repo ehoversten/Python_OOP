08ecf847305892677ec5e08d1dfc38c77b899ad2
ehoversten <ehoversten@gmail.com>
2018-05-01 17:04:16 -0400

    added Dog class

--- assignment_animal.py
+++ assignment_animal.py
@@ -7,7 +7,7 @@
 #
 # ------------------------------------------------------------------------------/
 
-class Animal(object):
+class Animal:
     def __init__(self, name, health):
         self.name = name
         self.health = health
@@ -24,11 +24,49 @@ class Animal(object):
         print("Health: " + str(self.health))
 
 
+# ---- Declare a Dog class that inherits from class Animal
+
+class Dog(Animal):
+    def __init__(self, name, health):
+        super().__init__(name, health)
+        if health > 150:
+            self.health = health
+        else:
+            self.health = 150
+
+    def pet(self):
+        self.health = self.health + 5
+        return self
+
+# ---- Declare a Dragon class that inherits from class Animal
+
+class Dragon(Animal):
+    def __init__(self, name, health):
+        super().__init__(name, health)
+        if health > 170:
+            self.health = health
+        else:
+            self.health = 170
+
+    def fly(self):
+        self.health = self.health - 10
+
+
 
 # ------------------------------------------------------------------------------/
+#
 #  Code to Test functionality of Animal class
+#
+# ------------------------------------------------------------------------------/
+
 
 testAnimal = Animal("Tom Cat", 50)
 testAnimal.displayHealth()
 testAnimal.walk().walk().walk().run().run().displayHealth()
 
+aDog = Dog("Marmaduke", 175)
+aDog.displayHealth()
+aDog.walk().walk().walk().run().run().pet().displayHealth()
+
+aDragon = Dragon("Falcore", 225)
+aDragon.displayHealth()
